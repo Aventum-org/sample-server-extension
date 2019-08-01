e8208ed724a72cbfbeb98f94e07093709dce41d5
Mohammed Al-Mahdawi <mohammed@al-mahdawi.com>
2019-08-01 22:26:04 +0300

    First commit

---
+++ aventum-sample-server-extension.js
@@ -0,0 +1,9 @@
+/*!
+ * Aventum Sample Server Extension v1.0.0
+ * (c) 2019 Mohammed Al-Mahdawi
+ * Released under the MIT License.
+ */
+
+const uuidv1 = require('uuid/v1')
+
+console.log('From Server ', uuidv1())
